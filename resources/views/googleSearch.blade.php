e137431661e6ee344ec2265774b120dbb0cfdd99
Jake Davies <jdavies.thfc@gmail.com>
2016-02-26 16:18:43 +0000

    Added Laravel HTML Collective to composer.json

--- resources/views/googleSearch.blade.php
+++ resources/views/googleSearch.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.master')
 
-@section('content')
-
-<h1>Google Search</h1>
-<p class="lead">Please supply name and any other data to the form.</p>
-<hr>
+@section('heading')
 
 @stop
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>People Tracker</title>
-<!-- TODO: local storage -->
+<!-- CDN, if preferred -->
 <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"> -->
-
-<script type="text/javascript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
 
 <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}" />
 
@@ -29,9 +27,25 @@
 
 <main>
     <div class="container">
-        @yield('content')
+        @yield('heading')
     </div>
 </main>
+
+<div class="alert alert-info" role="alert">Full name, e.g. Daniel Davies</div>
+<div class="input-group">
+  <span class="input-group-addon" id="basic-addon3">Person name</span>
+  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<div class="alert alert-info" role="alert">Please only provide one, e.g. Cardiff University</div>
+<div class="input-group">
+  <span class="input-group-addon" id="basic-addon3">School, address, age, location, etc.</span>
+  <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+</div>
+
+<button type="button" class="btn btn-default">Search</button>
+
+<script type="text/javascript" src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
 
 </body>
 </html>
