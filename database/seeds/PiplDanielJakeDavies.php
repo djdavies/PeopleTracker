ea6649bc265a059ecafea218c1c9902a05dc59fd
Jake Davies <jdavies.thfc@gmail.com>
2016-04-25 03:56:01 +0100

    in the midst of acquiring all the pilp json objects...

--- database/seeds/PiplDanielJakeDavies.php
+++ database/seeds/PiplDanielJakeDavies.php
@@ -31,44 +31,51 @@ class PiplDanielJakeDavies extends Seeder
 
             $peopleId = People::select('id')->where('name', '=', $name)->first();
 
-            // echo "PeopleId is: " . $peopleId;
+            // Perform the count here, so it's not in a loop iterating over a fairly large array, for performance.
+            // Excuse the 'magic number', it's simply because I always want only the first person, as it's the most likely hit. The other possible persons won't have an nth 
+            // array element to iterate, either. That is, some for say 'educations' will be
+            // blank, and it'll fail.
+ 			$addressesCount = count($result->possible_persons[0]->addresses);
+ 			$genderCount = count($result->possible_persons[0]->gender);
+ 			$jobsCount = count($result->possible_persons[0]->jobs);
+ 			$educationsCount = count($result->possible_persons[0]->educations);
 
             // Echo gender. 
             echo "**** GENDER ****\n";
-            for ($genderCount = 0; $genderCount < count($result->possible_persons[0]->gender); $genderCount++)
-            	print_r($result->possible_persons[0]->gender->content."\n");
+
+        	print_r($result->possible_persons[0]->gender->content."\n");
             echo "\n";
 
-            // Echo addresses. NB: [0] on addresses, I would need to loop through all of them.
-            echo "**** ADDRESSES ****\n";
-            for ($addressesCount = 0; $addressesCount < count($result->possible_persons[0]->addresses); $addressesCount++)
-            	print_r($result->possible_persons[0]->addresses[$addressesCount]->display."\n");
+            // Echo addresses. NB: [$possible_personsCount] on addresses, I would need to 	loop through all of them.
+            echo "**** ADDRESSES ****\n";	
+            for ($n = 0; $n < $addressesCount; $n++)
+            	print_r($result->possible_persons[0]->addresses[$n]->display."\n");
 			echo "\n";
 
 
             // Echo jobs.
             echo "**** JOBS ****\n";
-            for ($jobsCount = 0; $jobsCount < count($result->possible_persons[0]->jobs); $jobsCount++)
-            	print_r($result->possible_persons[0]->jobs[$jobsCount]->display."\n");
+            for ($n = 0; $n < $jobsCount; $n++)
+            	print_r($result->possible_persons[0]->jobs[$n]->display."\n");
             echo "\n";
 
             // Echo educations.
             echo "**** EDUCATIONS ****\n";
-            for ($educationsCount = 0; $educationsCount < count($result->possible_persons[0]->educations); $educationsCount++)
-            	print_r($result->possible_persons[0]->educations[$educationsCount]->display."\n");
-            echo "\n";
+            for ($n = 0; $n > $educationsCount; $n++); $educationsCount++)
+	   //          	print_r($result->possible_persons[$possible_personsCount]->educations[$educationsCount]->display."\n");
+	   //          echo "\n";
 
-            // Echo user_ids -- the google one is crazy.
-            echo "**** USER_IDS ****\n";
-            for ($user_idsCount = 0; $user_idsCount < count($result->possible_persons[0]->user_ids); $user_idsCount++)
-            	print_r($result->possible_persons[0]->user_ids[$user_idsCount]->content."\n");
-            echo "\n";
+	   //          // Echo user_ids -- the google one is crazy.
+	   //          echo "**** USER_IDS ****\n";
+	   //          for ($user_idsCount = 0; $user_idsCount < count($result->possible_persons[$possible_personsCount]->user_ids); $user_idsCount++)
+	   //          	print_r($result->possible_persons[$possible_personsCount]->user_ids[$user_idsCount]->content."\n");
+	   //          echo "\n";
 
-            // Echo images (url).
-            echo "**** IMAGES ****\n";
-            for ($imagesCount = 0; $imagesCount < count($result->possible_persons[0]->images); $imagesCount++)
-            	print_r($result->possible_persons[0]->images[$imagesCount]->url."\n");
-            echo "\n";
+	   //          // Echo images (url).
+	   //          echo "**** IMAGES ****\n";
+	   //          for ($imagesCount = 0; $imagesCount < count($result->possible_persons[$possible_personsCount]->images); $imagesCount++)
+	   //          	print_r($result->possible_persons[$possible_personsCount]->images[$imagesCount]->url."\n");
+	   //          echo "\n";
 
             // Iterate through objects and create results in DB.
             // for ($n = 0; $n < count($result->responseData->results); $n++) {
