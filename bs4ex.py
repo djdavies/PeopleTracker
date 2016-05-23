148c9b3085dc8702b3e0802fa90ab8a45565ace3
Jake Davies <jdavies.thfc@gmail.com>
2016-05-23 14:19:27 +0100

    Examples from the dissertation

---
+++ bs4ex.py
@@ -0,0 +1,13 @@
+html_doc = """
+<html><head><title>PeopleTracker BS4 Experiment</title></head>
+<body>
+<p class="title"><b>BS4 Experiment</b></p>
+
+<p class="experiment">Here I conduct a scraping experiment.
+<a href="http://example.com/exampleTarget1" class="target" id="link1">Target1</a>,
+<a href="http://example.com/exampleTarget2" class="target" id="link2">Target2</a> and
+<a href="http://example.com/exampleTargete" class="target" id="link3">Target3</a>;
+for an example in my final report..</p>
+
+<p class="experiment">...</p>
+"""
